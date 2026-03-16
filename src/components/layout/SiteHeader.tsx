ad7ed380a41353bacd4857c9ad7292bba261d2c6
이은석 <korea5410@gmail.com>
2026-03-16 18:24:13 +0900

    Revert "Place character images using Manus design reference"
    This reverts commit b19d9278588b6b551bdd01cda6ac4db07f5c47cb.

--- src/components/layout/SiteHeader.tsx
+++ src/components/layout/SiteHeader.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { publicNavigation, carrierNavigation } from "@/lib/constants/navigation";
@@ -23,7 +22,11 @@ export function SiteHeader({ settings }: { settings: SiteSettings }) {
             href="/"
             aria-label="홈으로 이동"
           >
-            <Image src="/images/characters/logo-dino.png" alt="인터넷공룡" width={180} height={48} className="h-10 w-auto md:h-12" priority />
+            <span
+              className="font-surround text-3xl font-black text-brand-orange"
+            >
+              인터넷공룡
+            </span>
           </Link>
 
           <div className="hidden items-center gap-8 lg:flex">
